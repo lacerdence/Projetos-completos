4d4cf7abefd8a78bc034b5cc9602b6751090bc08
lacerdence <vinniciuslacerda@yahoo.com.br>
2022-07-26 21:31:59 -0300

    Java_Swing_Campo_Minado

---
+++ Java/campo-minado-swing/src/br/com/avl/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,25 @@
+package br.com.avl.cm.visao;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+import br.com.avl.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+	
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+		setLayout(new GridLayout(tabuleiro.getLinhas(),
+				  tabuleiro.getColunas()));
+		
+		tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+		
+		tabuleiro.registrarObservador(e -> {
+			//TODO mostrar resultado para usuário !
+		});
+		}
+	}
+	
+
